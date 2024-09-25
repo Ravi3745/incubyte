f7f8e368b82f7f967d5229e22fe5cd690b349526
Ravi3745 <183037@nith.ac.in>
2024-09-26 01:23:56 +0530

    2. add numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,10 @@
 function add(numbers) {
     if (numbers === "") return 0;
+
+    const numArray = numbers.split(",");
+
+    const sum = numArray.reduce((curr, acc) => (curr += parseInt(acc)), 0);
+    return sum;
 }
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,6 +1,16 @@
 const { add } = require('./stringCalculator.js');
 
-// 1.test for empty string
-test('return 0 for an empty string', ()=> {
+// 1.test--> for empty string
+test('return 0 for an empty string', () => {
     expect(add("")).toBe(0);
+});
+
+// 2.test--> for string containing single number
+test('return number for single number string', () => {
+    expect(add("7")).toBe(7);
+});
+
+// 3.test--> sum of nummbers in  string seprated by commas ','
+test('return sum of two number string', () => {
+    expect(add("1,3")).toBe(4);
 });
